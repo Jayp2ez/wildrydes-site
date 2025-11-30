d918c4e047615a895a2aa45916a0ccfc94b4002b
Jayp2ez <104533841+Jayp2ez@users.noreply.github.com>
2025-11-30 12:54:51 -0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-west-1_Fj8qqLnrz
-        userPoolClientId: '', // e.g. 5i6m00n6btjgj7ls1mcfom026b
-        region: '' // e.g. us-west-1
+        userPoolId: 'us-west-1_Fj8qqLnrz', // e.g. us-west-1_Fj8qqLnrz
+        userPoolClientId: '5i6m00n6btjgj7ls1mcfom026b', // e.g. 5i6m00n6btjgj7ls1mcfom026b
+        region: 'us-west-1' // e.g. us-west-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
